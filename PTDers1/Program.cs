af39d3007fd51eae4c49f9210d85d8a78adea580
Doğukan ÖZEL <dogukanozel692@gmail.com>
2023-10-30 15:53:25 +0300

    PT Ders Uygulaması Ekledi

--- PTDers1/Program.cs
+++ PTDers1/Program.cs
@@ -20,13 +20,13 @@ namespace PTDers1
             Console.WriteLine(sayıkaresi);
             Console.ReadLine();
             */
-            
+            /*
             Console.Write("Sayı : ");
             int sayi = Convert.ToInt32(Console.ReadLine());
             int sonuc = sayi * sayi;
             Console.WriteLine(sayi+ " - Değerinin karesi : " + sonuc);
             Console.ReadKey();
-
+            */
             #endregion
 
             #region Sözlük ÖDEVİ.
@@ -39,6 +39,7 @@ namespace PTDers1
              * Var olan bir key değeri ekliyor ise kullanıcıya bu değer daha 
              * önceden ... şeklinde sistemimizde bulunmaktadır.
              */
+            /*
             Hashtable sozlukVeriTabani = new Hashtable();
 
             do
@@ -76,7 +77,66 @@ namespace PTDers1
                 //Console.WriteLine(item.GetType().Name); 
                 Console.WriteLine("ENG : {0} = {1} : TR", item.Key, item.Value);
             }
+            */
+            #endregion
 
+            #region Memur maaş katsayısını tutan değişken
+            /*
+            double mmk2 = 0.509;
+            byte not;
+            not = 98;
+            short s1, s2 = 90, s3;
+            s1 = 50;
+            not = 50;
+            const double pi=0.314;
+            //pi = 3.18; //sabitin değeri değiştirilemez
+
+            string isim = "doğukan";
+            double mks= 1.25;
+            int sayım = 32;
+            decimal kesirli = 10/2;
+            short sayım2 = 2;
+            byte yas = 23;
+            const int boy = 175;
+
+            int sayi1 = 613; //atama işlemi
+            sayi1 = 25 + 15; //aritmetiksel işlem
+            int sa1 = 12, sa2 = 10;
+            sa2 += sa1;
+            sa2 *= sa1;
+
+            sa1++;
+            sa2 = sa1++;//sa1 = 14, sa2=13
+            sa2 = ++sa1; //sa1 = 15, sa2=15
+
+            sa1--;
+            sa1 = sa2--;
+            sa1 = --sa2;
+            */
+            #endregion
+
+            #region PT Ders Uygulama
+            //SORU : Klavyeden girilen iki sayının ortalamasını ekrana yazdıran kodu yazınız.
+
+            Console.Write("Sayı 1 : ");
+            int s1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Sayı 2 : ");
+            int s2 = Convert.ToInt32(Console.ReadLine());
+
+            int toplam = s1 + s2;
+            double bolum = toplam / 2;
+            Console.WriteLine("Girilen değerler {0} ve {1}" ,s1 , s2 );
+            Console.WriteLine("Ortalaması : " + bolum);
+            Console.ReadLine();
+
+            //2.Yöntem
+            /*
+            int sayıilk = Convert.ToInt32(Console.ReadLine());
+            int sayıiki = Convert.ToInt32(Console.ReadLine());
+
+            double ortalama = (sayıilk + sayıiki) / 2;
+            Console.WriteLine(ortalama);
+            */
             #endregion
 
         }
