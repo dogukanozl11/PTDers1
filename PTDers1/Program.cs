e751e4c1dfeb9df15ff911bb96fd06d9885b2c51
Doğukan ÖZEL <dogukanozel692@gmail.com>
2023-10-30 15:55:12 +0300

    Güncelleme

--- PTDers1/Program.cs
+++ PTDers1/Program.cs
@@ -12,21 +12,21 @@ namespace PTDers1
         static void Main(string[] args)
         {
             #region Sayının Karesini Alma
-            /*
+            
             Console.Write("Lütfen karesini almak istediğniz Sayıyı girniz : ");
             string sayı = Console.ReadLine();
             int sayıint = Convert.ToInt32(sayı);
             int sayıkaresi = sayıint * sayıint;
             Console.WriteLine(sayıkaresi);
             Console.ReadLine();
-            */
-            /*
+            
+            
             Console.Write("Sayı : ");
             int sayi = Convert.ToInt32(Console.ReadLine());
             int sonuc = sayi * sayi;
             Console.WriteLine(sayi+ " - Değerinin karesi : " + sonuc);
             Console.ReadKey();
-            */
+            
             #endregion
 
             #region Sözlük ÖDEVİ.
